45086318d991282e5ba85b4657af1b66c679ebf8
Dung Pham Anh <dphamanh45@gmail.com>
2025-11-10 15:44:25 +0700

    add favicon and rename file in bash job

--- cronjob/run_gold_price.sh
+++ cronjob/run_gold_price.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/dungpham/me/gold-price-tracker
-python3 scripts/get_data_vnexpress.py >> cronjob/logs/gold_price.log 2>&1
+cd /home/dungpa/gold-price-tracker
+python3 scripts/collect_data_vnexpress.py >> cronjob/logs/gold_price.log 2>&1
